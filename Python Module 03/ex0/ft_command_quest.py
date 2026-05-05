0254e21e81a0a231c9f876fde47b764930abd45b
Davit Manukyan <dmanukyan004@gmail.com>
2026-05-05 10:38:54 +0400

    Save work before pull

--- Python Module 03/ex0/ft_command_quest.py
+++ Python Module 03/ex0/ft_command_quest.py
@@ -1,17 +1,18 @@
 import sys
 
 def show_data():
-	print("=== Command Quest ===")
-	print(f"Program Name: {sys.argv[0]}")
-	if len(sys.argv) == 1:
-		print("No Arguments Provided!")
-	else:
-		i = 1
-		print (f"Arguments Recieved: {len(sys.argv) - 1}")
-		while i < len(sys.argv):
-			print(f"Argument {i}: {sys.argv[i]}")
-			i += 1
-	print(f"Total Arguments: {len(sys.argv)}")
+    print("=== Command Quest ===")
+    print(f"Program Name: {sys.argv[0]}")
+    if len(sys.argv) == 1:
+        print("No Arguments Provided!")
+    else:
+        i = 1
+        print (f"Arguments Recieved: {len(sys.argv) - 1}")
+        while i < len(sys.argv):
+            print(f"Argument {i}: {sys.argv[i]}")
+            i += 1
+    print(f"Total Arguments: {len(sys.argv)}")
+
 
 if __name__ == "__main__":
-	show_data()
+    show_data()
